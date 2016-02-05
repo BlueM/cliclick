276f349324e27ba49a9b7f2e80c4fbb6cc06cecc
王超(技术02) <wangchaojs02@dangdang.com>
2016-02-05 14:04:40 +0800

    kp添加对a-z的支持

--- Actions/KeyPressAction.m
+++ Actions/KeyPressAction.m
@@ -27,6 +27,7 @@
  */
 
 #import "KeyPressAction.h"
+#import "KeycodeInformer.h"
 
 @implementation KeyPressAction
 
@@ -44,8 +45,16 @@
 }
 
 #pragma mark - KeyBaseAction
++(NSString*)keyCodesForCharacter:(NSString*)character
+{
+    KeycodeInformer *ki = [KeycodeInformer sharedInstance];
+    NSArray* keyCodeInfo = [ki keyCodesForString:character].firstObject;
+    NSString* keyCode = [keyCodeInfo objectAtIndex:0];
+    return keyCodeInfo?keyCode: @"-1";
+}
 
 +(NSDictionary *)getSupportedKeycodes {
+
     return [NSDictionary dictionaryWithObjectsAndKeys:
             @"36", @"return",
             @"76", @"enter",
@@ -90,6 +99,103 @@
             [NSString stringWithFormat:@"%i", NX_KEYTYPE_ILLUMINATION_TOGGLE],  @"keys-light-toggle",
             [NSString stringWithFormat:@"%i", NX_KEYTYPE_ILLUMINATION_UP],  @"keys-light-up",
             [NSString stringWithFormat:@"%i", NX_KEYTYPE_ILLUMINATION_DOWN],  @"keys-light-down",
+
+            // Char
+            [self keyCodesForCharacter:@"!"], @"!",
+            [self keyCodesForCharacter:@"\""], @"\"",
+            [self keyCodesForCharacter:@"#"], @"#",
+            [self keyCodesForCharacter:@"$"], @"$",
+            [self keyCodesForCharacter:@"%"], @"%",
+            [self keyCodesForCharacter:@"&"], @"&",
+            [self keyCodesForCharacter:@"'"], @"'",
+            [self keyCodesForCharacter:@"("], @"(",
+            [self keyCodesForCharacter:@")"], @")",
+            [self keyCodesForCharacter:@"*"], @"*",
+            [self keyCodesForCharacter:@"+"], @"+",
+            [self keyCodesForCharacter:@","], @",",
+            [self keyCodesForCharacter:@"-"], @"-",
+            [self keyCodesForCharacter:@"."], @".",
+            [self keyCodesForCharacter:@"/"], @"/",
+            [self keyCodesForCharacter:@"0"], @"0",
+            [self keyCodesForCharacter:@"1"], @"1",
+            [self keyCodesForCharacter:@"2"], @"2",
+            [self keyCodesForCharacter:@"3"], @"3",
+            [self keyCodesForCharacter:@"4"], @"4",
+            [self keyCodesForCharacter:@"5"], @"5",
+            [self keyCodesForCharacter:@"6"], @"6",
+            [self keyCodesForCharacter:@"7"], @"7",
+            [self keyCodesForCharacter:@"8"], @"8",
+            [self keyCodesForCharacter:@"9"], @"9",
+            [self keyCodesForCharacter:@":"], @":",
+            [self keyCodesForCharacter:@";"], @";",
+            [self keyCodesForCharacter:@"<"], @"<",
+            [self keyCodesForCharacter:@"="], @"=",
+            [self keyCodesForCharacter:@">"], @">",
+            [self keyCodesForCharacter:@"?"], @"?",
+            [self keyCodesForCharacter:@"@"], @"@",
+            [self keyCodesForCharacter:@"A"], @"A",
+            [self keyCodesForCharacter:@"B"], @"B",
+            [self keyCodesForCharacter:@"C"], @"C",
+            [self keyCodesForCharacter:@"D"], @"D",
+            [self keyCodesForCharacter:@"E"], @"E",
+            [self keyCodesForCharacter:@"F"], @"F",
+            [self keyCodesForCharacter:@"G"], @"G",
+            [self keyCodesForCharacter:@"H"], @"H",
+            [self keyCodesForCharacter:@"I"], @"I",
+            [self keyCodesForCharacter:@"J"], @"J",
+            [self keyCodesForCharacter:@"K"], @"K",
+            [self keyCodesForCharacter:@"L"], @"L",
+            [self keyCodesForCharacter:@"M"], @"M",
+            [self keyCodesForCharacter:@"N"], @"N",
+            [self keyCodesForCharacter:@"O"], @"O",
+            [self keyCodesForCharacter:@"P"], @"P",
+            [self keyCodesForCharacter:@"Q"], @"Q",
+            [self keyCodesForCharacter:@"R"], @"R",
+            [self keyCodesForCharacter:@"S"], @"S",
+            [self keyCodesForCharacter:@"T"], @"T",
+            [self keyCodesForCharacter:@"U"], @"U",
+            [self keyCodesForCharacter:@"V"], @"V",
+            [self keyCodesForCharacter:@"W"], @"W",
+            [self keyCodesForCharacter:@"X"], @"X",
+            [self keyCodesForCharacter:@"Y"], @"Y",
+            [self keyCodesForCharacter:@"Z"], @"Z",
+            [self keyCodesForCharacter:@"["], @"[",
+            [self keyCodesForCharacter:@"\\"], @"\\",
+             [self keyCodesForCharacter:@"]"], @"]",
+             [self keyCodesForCharacter:@"^"], @"^",
+             [self keyCodesForCharacter:@"_"], @"_",
+             [self keyCodesForCharacter:@"`"], @"`",
+             [self keyCodesForCharacter:@"a"], @"a",
+             [self keyCodesForCharacter:@"b"], @"b",
+             [self keyCodesForCharacter:@"c"], @"c",
+             [self keyCodesForCharacter:@"d"], @"d",
+             [self keyCodesForCharacter:@"e"], @"e",
+             [self keyCodesForCharacter:@"f"], @"f",
+             [self keyCodesForCharacter:@"g"], @"g",
+             [self keyCodesForCharacter:@"h"], @"h",
+             [self keyCodesForCharacter:@"i"], @"i",
+             [self keyCodesForCharacter:@"j"], @"j",
+             [self keyCodesForCharacter:@"k"], @"k",
+             [self keyCodesForCharacter:@"l"], @"l",
+             [self keyCodesForCharacter:@"m"], @"m",
+             [self keyCodesForCharacter:@"n"], @"n",
+             [self keyCodesForCharacter:@"o"], @"o",
+             [self keyCodesForCharacter:@"p"], @"p",
+             [self keyCodesForCharacter:@"q"], @"q",
+             [self keyCodesForCharacter:@"r"], @"r",
+             [self keyCodesForCharacter:@"s"], @"s",
+             [self keyCodesForCharacter:@"t"], @"t",
+             [self keyCodesForCharacter:@"u"], @"u",
+             [self keyCodesForCharacter:@"v"], @"v",
+             [self keyCodesForCharacter:@"w"], @"w",
+             [self keyCodesForCharacter:@"x"], @"x",
+             [self keyCodesForCharacter:@"y"], @"y",
+             [self keyCodesForCharacter:@"z"], @"z",
+             [self keyCodesForCharacter:@"{"], @"{",
+             [self keyCodesForCharacter:@"|"], @"|",
+             [self keyCodesForCharacter:@"}"], @"}",
+             [self keyCodesForCharacter:@"~"], @"~",
+
             nil];
 }
 
