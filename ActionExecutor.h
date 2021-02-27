31e4f3b0108a26c6cd2335c9c9e9dcf05c1567a9
Carsten Blüm <carsten@bluem.net>
2021-02-27 16:10:40 +0100

    Update year in copyright notice

--- ActionExecutor.h
+++ ActionExecutor.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2007-2018, Carsten Blüm <carsten@bluem.net>
+ * Copyright (c) 2007-2021, Carsten Blüm <carsten@bluem.net>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
